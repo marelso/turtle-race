7e2020f66a31bb7195d90e712f713e8265793a96
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-03-04 09:45:30 -0300

    refactor create turtle function to accept params, including turn that receives turtles and start race

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from turtle import *
+from random import randint
 
 drawing_speed = 10
 
@@ -19,18 +20,32 @@ def draw_speedway():
         forward(20)
 
 
-def create_turtle():
-    ada = Turtle()
-    ada.color('red')
-    ada.shape('turtle')
-    ada.penup()
-    ada.goto(-160, 100)
-    ada.pendown()
+def create_turtle(color, x, y):
+    turtle = Turtle()
+    turtle.color(color)
+    turtle.shape('turtle')
+    turtle.penup()
+    turtle.goto(x, y)
+    turtle.pendown()
+    return turtle
+
+
+def turn(turtles):
+    for turn in range(100):
+        turtles[0].forward(randint(1,5))
+        turtles[1].forward(randint(1,5))
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    turtles = []
     draw_speedway()
-    create_turtle()
 
+    erica = create_turtle('green', -160, 100)
+    marelso = create_turtle('blue', -160, 70)
+
+    turtles.append(erica)
+    turtles.append(marelso)
+
+    turn(turtles)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
