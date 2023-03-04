49d989ad77758c884687497b0a6dfc9a55278238
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-03-04 09:31:53 -0300

    Include draw speedway method

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
+from turtle import *
+
+
+def draw_speedway():
+    speed(5)
+    penup()
+    goto(-140, 140)
+    for step in range(16):
+        write(step, align='center')
+        right(90)
+        forward(10)
+        pendown()
+        forward(150)
+        penup()
+        backward(160)
+        left(90)
+        forward(20)
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
