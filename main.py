330ca12db28af4aed740d903f423eabadfaa86fe
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-03-04 09:32:57 -0300

    Include add turtle method

--- main.py
+++ main.py
@@ -17,6 +17,18 @@ def draw_speedway():
         forward(20)
 
 
+def create_turtle():
+    ada = Turtle()
+    ada.color('red')
+    ada.shape('turtle')
+    ada.penup()
+    ada.goto(-160, 100)
+    ada.pendown()
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    draw_speedway()
+
+
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
