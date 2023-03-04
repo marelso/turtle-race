a748eb5f2f61a11b2ec14ed073a3d4bbf0a1636f
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-03-04 11:08:23 -0300

    Include new interface with tk inter, now its possible to pass the number of competitors and then select the colors.

--- main.py
+++ main.py
@@ -2,6 +2,56 @@ from turtle import *
 from random import randint
 import tkinter as tk
 
+
+class NumberField(tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.number_var = tk.StringVar()
+
+        tk.Label(self, text="Number:").grid(row=0, column=0)
+        tk.Entry(self, textvariable=self.number_var).grid(row=0, column=1)
+
+        tk.Button(self, text="Next", command=self.show_text_fields).grid(row=1, column=0, columnspan=2, pady=10)
+
+    def show_text_fields(self):
+        num = int(self.number_var.get())
+        self.master.withdraw()  # hide NumberField window
+        TextFields(self.master, num, self.on_start)
+
+    def on_start(self, values):
+        print(f"Start button clicked with values: {values}")
+
+        turtles = start(values)
+
+        draw_speedway()
+
+        turn(turtles)
+        # ... do something with values ...
+
+
+class TextFields(tk.Toplevel):
+    def __init__(self, parent, num, on_start):
+        super().__init__(parent)
+
+        self.text_vars = []
+        self.on_start = on_start
+
+        for i in range(num):
+            var = tk.StringVar()
+            tk.Label(self, text=f"Text {i + 1}:").grid(row=i, column=0)
+            tk.Entry(self, textvariable=var).grid(row=i, column=1)
+            self.text_vars.append(var)
+
+        tk.Button(self, text="Start", command=self.do_start).grid(row=num, column=0, columnspan=2, pady=10)
+
+    def do_start(self):
+        values = [var.get() for var in self.text_vars]
+        self.on_start(values)
+        self.destroy()  # close TextFields window
+        self.master.deiconify()  # show NumberField window again
+
+
 drawing_speed = 20
 
 
@@ -41,8 +91,8 @@ def start(competitors):
     base = 100
     turtles = []
 
-    for id in range(competitors):
-        color = input("Choose a color for the turtle:")
+    for id in range(len(competitors)):
+        color = competitors[id]
 
         turtles.append(create_turtle(color, [-160, base]))
         base -= 30
@@ -52,9 +102,11 @@ def start(competitors):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    turtles = start(int(input("Choose the number of competitors:")))
+    root = tk.Tk()
+    root.geometry("300x200")
 
-    draw_speedway()
+    number_field = NumberField(root)
+    number_field.pack(fill="both", expand=True)
 
-    turn(turtles)
+    root.mainloop()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
